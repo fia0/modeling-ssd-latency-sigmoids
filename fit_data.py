c3ddd6bfc8f747eb5b87c44586e8013b32d5bef2
fia <fia@spacesnek.rocks>
2024-10-25 16:00:41 +0200

    add output as csv

--- fit_data.py
+++ fit_data.py
@@ -1,10 +1,31 @@
 #!/bin/env python
-
 from scipy.optimize import curve_fit
 import pandas as pd
 import math
 import numpy
 import matplotlib.pyplot as plt
+
+"""
+This scripts does:
+    - Find an approximated function for IO latency count described by
+      output.csv
+    - Inverse a normalized version of this approximation to find the relation
+      percentile -> latency with another approximation
+
+The input is a csv file created by fio accompanying
+`tools/fio_jsonplus_clat2csv` script.
+
+The inverse function is a single logit given with 5 constants which are
+determined in the script:
+
+h(x) = a * log((x*c+d)/(1-x+e)) + b
+
+The result of this function is the time of the given percentile in nanoseconds.
+
+The results is given as another csv with the following format:
+
+blocksize, rwratio, a, b, c, d, e
+"""
 
 def sigmoid(x, alpha, beta, gamma):
     return alpha / (1 + math.e**(beta * (math.log(x) - gamma)))
@@ -15,7 +36,7 @@ def sigmoids(xs, alpha, beta, gamma):
 def double_sigmoids(xs, a0, b0, c0, a1, b1, c1):
     return numpy.array([sigmoid(x, a0, b0, c0) for x in xs]) + numpy.array([sigmoid(x, a1, b1, c1) for x in xs])
 
-data = pd.read_csv("output.csv", skipinitialspace=True)
+data = pd.read_csv("input.csv", skipinitialspace=True)
 data = data[['nsec', 'read_clat_ns_count', 'read_clat_ns_cumulative', 'read_clat_ns_percentile','write_clat_ns_count', 'write_clat_ns_cumulative', 'write_clat_ns_percentile',]]
 
 def sigmoid_error(s):
@@ -39,6 +60,17 @@ res_curve, cov = curve_fit(
     ),
 )
 
+double_res_curve, _ = curve_fit(
+    double_sigmoids,
+    data['nsec'].to_numpy(),
+    data['read_clat_ns_cumulative'].to_numpy(),
+    [*res_curve, 0, 0, math.log(data['nsec'].min())],
+    bounds = (
+        lower_bound() + lower_bound(),
+        upper_bound() + upper_bound(),
+    ),
+)
+
 # we got f(x) now
 #
 # let's get g(x) next
@@ -53,6 +85,8 @@ def normalized_sigmoid(x):
 # bounded_res = approx_res / (upper_bound - lower_bound)
 
 # with pynverse
+# import pynverse
+#
 #inverse_normalized_sigmoid = pynverse.inversefunc(normalized_sigmoid, domain=[data['nsec'].min(), data['nsec'].max()])
 
 def inverse_sigmoid(x, alpha, beta, gamma, delta, epsilon):
@@ -111,6 +145,29 @@ axs[0][2].set_xlabel("Percentiles")
 axs[0][2].set_ylabel("Time [nsec]")
 
 axs[1][0].scatter(data['nsec'], data['read_clat_ns_cumulative'], label="Reference")
+axs[1][0].plot(data['nsec'], double_sigmoids(data['nsec'], *double_res_curve), label="CF")
 
 fig.legend()
 fig.savefig("output.svg")
+
+
+with open("output.csv", 'w', encoding="utf-8") as file:
+    file.write("blocksize,rwratio,a,b,c,d,e\n")
+    file.write("131072,1.0")
+    for x in inv_curve:
+        file.write(f",{x}")
+    file.write("\n")
+
+
+print("f(x):")
+print(f"\t alpha: {res_curve[0]}")
+print(f"\t beta: {res_curve[1]}")
+print(f"\t gamma: {res_curve[2]}")
+
+print("h(x):")
+print(f"\t alpha: {inv_curve[0]}")
+print(f"\t beta: {inv_curve[1]}")
+print(f"\t gamma: {inv_curve[2]}")
+print(f"\t delta: {inv_curve[3]}")
+print(f"\t epsilon: {inv_curve[4]}")
+
